2dc6d48098b146868c3d9d20900b861985cba9da
MuhamedShahabas <muhamedshahabasc@gmail.com>
2023-07-11 17:35:49 +0530

    Added ENV to Netlify

--- src/config/firbase.js
+++ src/config/firbase.js
@@ -10,13 +10,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MSID,
-  appId: import.meta.env.VITE_ID,
-  measurementId: import.meta.env.VITE_MEASUREID,
+  apiKey: process.env.VITE_APIKEY,
+  authDomain: process.env.VITE_AUTHDOMAIN,
+  projectId: process.env.VITE_PROJECTID,
+  storageBucket: process.env.VITE_STORAGEBUCKET,
+  messagingSenderId: process.env.VITE_MSID,
+  appId: process.env.VITE_ID,
+  measurementId: process.env.VITE_MEASUREID,
 };
 
 // Initialize Firebase
